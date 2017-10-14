e9282aef58d3990036c46767f6bdac6a1c9a0b40
Benjamin Chambers <benjamindchambers@outlook.com>
2017-10-14 10:00:47 -0600

    Added the ability to shift all stones on the board by X number of spaces

--- Gomoku/Gomoku/Board.cs
+++ Gomoku/Gomoku/Board.cs
@@ -16,6 +16,43 @@ namespace Gomoku
             State = Source.State;
         }
 
+        public Board Shift(int dX, int dY)
+        {
+            var result = new Board();
+
+            for (int c=Math.Max(0, dX); c<Math.Min(15,dX); c++)
+            {
+                for (int r=Math.Max(0,dY); r<Math.Min(15,dY); r++)
+                {
+                    result.data[c, r] = this[c + dX, r + dY];
+                }
+            }
+
+            return result;
+        }
+
+        public (int MinC, int MaxC, int MinR, int MaxR) GetWhiteSpace()
+        {
+            int minC = 0, maxC = 0;
+            int minR = 0, maxR = 0;
+
+            for (int c = 0; c < 15; c++)
+            {
+                for (int r = 0; r < 15; r++)
+                {
+                    if (this[c, r] != Stone.Empty)
+                    {
+                        minC = Math.Min(c, minC);
+                        maxC = Math.Max(c, maxC);
+                        minR = Math.Min(r, minR);
+                        maxR = Math.Max(r, maxR);
+                    }
+                }
+            }
+
+            return (minC, maxC, minR, maxR);
+        }
+
         public Board Put(int Column, int Row, Stone Color)
         {
             if (Color == Stone.Empty)
@@ -23,7 +60,7 @@ namespace Gomoku
 
             var result = new Board(this);
             result.data[Column, Row] = Color;
-            result.checkState(Column, Row);
+            result.CheckState(Column, Row);
             return result;
         }
 
@@ -41,14 +78,14 @@ namespace Gomoku
             private set;
         }
 
-        private void checkState(int Column, int Row)
+        private void CheckState(int Column, int Row)
         {
             var which = this[Column, Row];
 
-            int d19 = crawl(Column, Row, which, -1, -1) + crawl(Column, Row, which, 1, 1);
-            int d28 = crawl(Column, Row, which, 0, -1) + crawl(Column, Row, which, 0, 1);
-            int d37 = crawl(Column, Row, which, -1, 1) + crawl(Column, Row, which, 1, -1);
-            int d46 = crawl(Column, Row, which, -1, 0) + crawl(Column, Row, which, 1, 0);
+            int d19 = Crawl(Column, Row, which, -1, -1) + Crawl(Column, Row, which, 1, 1);
+            int d28 = Crawl(Column, Row, which, 0, -1) + Crawl(Column, Row, which, 0, 1);
+            int d37 = Crawl(Column, Row, which, -1, 1) + Crawl(Column, Row, which, 1, -1);
+            int d46 = Crawl(Column, Row, which, -1, 0) + Crawl(Column, Row, which, 1, 0);
 
             if ((d19>5) || (d28>5) || (d37>5) || (d46>5))
             {
@@ -60,7 +97,7 @@ namespace Gomoku
             }
         }
 
-        private int crawl(int column, int row, Stone color, int dc, int dr)
+        private int Crawl(int column, int row, Stone color, int dc, int dr)
         {
             int result = 0;
             
