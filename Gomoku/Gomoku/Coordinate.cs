8937f9204b3d8319055cbdc09ee4efa73edcbc04
Benjamin Chambers <benjamindchambers@outlook.com>
2017-10-15 09:16:49 -0600

    Added a Coordinate struct

---
+++ Gomoku/Gomoku/Coordinate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gomoku
+{
+    public struct Coordinate
+    {
+        public int Column;
+        public int Row;
+    }
+}
