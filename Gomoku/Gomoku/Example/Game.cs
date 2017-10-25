078fc208906be6b66019f6fa4bbe9bd12b8d1d8f
Benjamin Chambers <benjamindchambers@outlook.com>
2017-10-25 11:01:36 -0600

    Added an example of a Game class that tracks move history

---
+++ Gomoku/Gomoku/Example/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gomoku.Example
+{
+    public class Game
+    {
+        public Game() { }
+
+        public Board CurrentBoard { get; private set; }
+        public IReadOnlyList<(Stone Color, Coordinate Where, Board Result)> GetHistory() { return History; }
+
+        public void PlayMove(Coordinate Location)
+        {
+            if (CurrentBoard.State == BoardState.InProgress)
+            {
+                Stone player = (History.Count % 2 == 0) ? Stone.Black : Stone.White;
+                CurrentBoard = CurrentBoard.Put(Location, player);
+                History.Add((player, Location, CurrentBoard));
+            }
+        }
+
+        List<(Stone Color, Coordinate Where, Board Result)> History = new List<(Stone Color, Coordinate Where, Board Result)>();
+    }
+}
